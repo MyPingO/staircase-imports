5d4180e79dc3924c734f2edada94041a72ed39fe
MyPing0 <myping0yt@gmail.com>
2024-02-23 18:58:55 -0500

    Add Python formatter test suite -All tests pass!

---
+++ test/pythonFormatter.test.js
@@ -0,0 +1,215 @@
+const assert = require("assert");
+const vscode = require("vscode");
+// The following simulates testing a Python file within a JavaScript testing environment.
+
+async function createOrVerifyFile(filePath) {
+	try {
+		await vscode.workspace.fs.writeFile(filePath, Buffer.from("")); // Create or overwrite
+		console.log("File is ready (created or already existed).");
+	} catch (err) {
+		console.error("Error handling file:", err);
+	}
+}
+
+suite("Extension Test Suite", () => {
+	vscode.window.showInformationMessage("Start all tests.");
+
+	test("Basic Python Imports Test", async () => {
+		const filePath = vscode.Uri.file(__dirname + "/test.py");
+		await createOrVerifyFile(filePath);
+
+		const document = await vscode.workspace.openTextDocument(filePath);
+		const editor = await vscode.window.showTextDocument(document);
+
+		// Python content for testing
+		const content = `
+        from test import test
+        from TEST import TEST
+        from Tt import T
+        `;
+
+		await editor.edit((editBuilder) => {
+			editBuilder.replace(new vscode.Range(0, 0, document.lineCount, 0), content);
+		});
+
+		await document.save();
+
+		const expectedContent = `
+        from Tt import T
+        from test import test
+        from TEST import TEST
+        `;
+
+		const documentContent = document.getText().replace(/\r/g, "");
+
+		assert.strictEqual(documentContent, expectedContent);
+
+		await vscode.commands.executeCommand("workbench.action.closeActiveEditor");
+	});
+
+	test("Basic Imports Test with Leading Comments", async () => {
+		const filePath = vscode.Uri.file(__dirname + "/test.py");
+		await createOrVerifyFile(filePath);
+
+		const document = await vscode.workspace.openTextDocument(filePath);
+		const editor = await vscode.window.showTextDocument(document);
+
+		// Python content with comments
+		const content = `
+        # I am a part of test
+        from test import test
+        # I am a part of TEST
+        from TEST import TEST
+        # I am a part of T
+        from Tt import T
+        `;
+
+		await editor.edit((editBuilder) => {
+			editBuilder.replace(new vscode.Range(0, 0, document.lineCount, 0), content);
+		});
+
+		await document.save();
+
+		const expectedContent = `
+        # I am a part of T
+        from Tt import T
+        # I am a part of test
+        from test import test
+        # I am a part of TEST
+        from TEST import TEST
+        `;
+
+		const documentContent = document.getText().replace(/\r/g, "");
+
+		assert.strictEqual(documentContent, expectedContent);
+
+		await vscode.commands.executeCommand("workbench.action.closeActiveEditor");
+	});
+
+	test("Multiline Python Imports Test", async () => {
+		const filePath = vscode.Uri.file(__dirname + "/test.py");
+		await createOrVerifyFile(filePath);
+
+		const document = await vscode.workspace.openTextDocument(filePath);
+		const editor = await vscode.window.showTextDocument(document);
+
+		// Multiline Python imports
+		const content = `
+        from TEST import (
+            TEST,
+            T, #lol
+            test,
+        )
+        `;
+
+		await editor.edit((editBuilder) => {
+			editBuilder.replace(new vscode.Range(0, 0, document.lineCount, 0), content);
+		});
+
+		await document.save();
+
+		const expectedContent = `
+        from TEST import (
+            TEST,
+            test,
+            T, #lol
+        )
+        `;
+
+		const documentContent = document.getText().replace(/\r/g, "");
+
+		assert.strictEqual(documentContent, expectedContent);
+
+		await vscode.commands.executeCommand("workbench.action.closeActiveEditor");
+	});
+
+	test("Python Multiline Imports Test with Leading Comments", async () => {
+		const filePath = vscode.Uri.file(__dirname + "/test.py");
+		await createOrVerifyFile(filePath);
+
+		const document = await vscode.workspace.openTextDocument(filePath);
+		const editor = await vscode.window.showTextDocument(document);
+
+		// Python multiline imports with comments
+		const content = `
+        # This is a comment
+        from TEST import (
+            # I am part of TEST
+            TEST,
+            # I am part of T
+            T, #}
+            # I am part of test
+            test
+        )
+        `;
+
+		await editor.edit((editBuilder) => {
+			editBuilder.replace(new vscode.Range(0, 0, document.lineCount, 0), content);
+		});
+
+		await document.save();
+
+		const expectedContent = `
+        # This is a comment
+        from TEST import (
+            # I am part of TEST
+            TEST,
+            # I am part of T
+            T, #}
+            # I am part of test
+            test,
+        )
+        `;
+
+		const documentContent = document.getText().replace(/\r/g, "");
+
+		assert.strictEqual(documentContent, expectedContent);
+
+		await vscode.commands.executeCommand("workbench.action.closeActiveEditor");
+	});
+
+	test("Python Multiline Imports Test with Trailing Comments", async () => {
+		const filePath = vscode.Uri.file(__dirname + "/test.py");
+		await createOrVerifyFile(filePath);
+
+		const document = await vscode.workspace.openTextDocument(filePath);
+		const editor = await vscode.window.showTextDocument(document);
+
+		// Python multiline imports with comments
+		const content = `
+        import TEST as T # This is a comment
+        import T # This is a comment
+        from test import test # This is a comment
+
+        from TEST import (
+            TEST, # This is a comment
+            T, # This is a comment
+            test # This is a comment
+        )
+        `;
+
+		await editor.edit((editBuilder) => {
+			editBuilder.replace(new vscode.Range(0, 0, document.lineCount, 0), content);
+		});
+
+		await document.save();
+
+		const expectedContent = `
+        import T # This is a comment
+        import TEST as T # This is a comment
+        from test import test # This is a comment
+
+        from TEST import (
+            T, # This is a comment
+            TEST, # This is a comment
+            test, # This is a comment
+        )
+        `;
+
+		const documentContent = document.getText().replace(/\r/g, "");
+
+		assert.strictEqual(documentContent, expectedContent);
+
+		await vscode.commands.executeCommand("workbench.action.closeActiveEditor");
+	});
+});
